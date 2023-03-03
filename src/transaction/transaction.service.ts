8aa7924ff8cc56a78ce9a5ea6ad2431abab9eb24
Taras Makarchuk <2278452@gmail.com>
2023-03-04 00:38:52 +0200

    :hammer: add create, get with pagination, delete transaction endpoints

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,9 +1,10 @@
 import { TransactionEntity } from './entity/transaction.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { from, Observable } from 'rxjs';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { DeleteResult, Repository } from 'typeorm';
+import { from, map, Observable, switchMap } from 'rxjs';
 import { TransactionDto } from './dto/transaction.dto';
+import { OrderBy } from '../banks/enum/order-by.enum';
 
 @Injectable()
 export class TransactionService {
@@ -14,5 +15,44 @@ export class TransactionService {
 
   createTransaction(dto: TransactionDto): Observable<TransactionEntity> {
     return from(this.transactionRepository.save(dto));
+  }
+
+  findTransactions(
+    take: number,
+    skip: number,
+    sortedField: string,
+    orderBy: OrderBy,
+  ): Observable<TransactionEntity[]> {
+    return from(
+      this.transactionRepository
+        .createQueryBuilder('transactions')
+        .orderBy(`transactions.${sortedField}`, `${orderBy}`)
+        .take(take)
+        .skip(skip)
+        .getMany(),
+    );
+  }
+
+  findTransactionById(id: number): Observable<TransactionEntity> {
+    return from(
+      this.transactionRepository.findOne({
+        where: [{ id }],
+      }),
+    ).pipe(
+      map((transaction: TransactionEntity) => {
+        if (!transaction) {
+          throw new HttpException('Transactions not found', HttpStatus.NOT_FOUND);
+        }
+        return transaction;
+      }),
+    );
+  }
+
+  removeTransaction(id: number): Observable<DeleteResult> {
+    return from(this.findTransactionById(id)).pipe(
+      switchMap((transaction: TransactionEntity) => {
+        return this.transactionRepository.delete(transaction.id);
+      }),
+    );
   }
 }
