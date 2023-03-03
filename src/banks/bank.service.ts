14ff19bcd00b0ddfa03847716df3bb17d292335a
Taras Makarchuk <2278452@gmail.com>
2023-03-03 23:31:57 +0200

    :white_check_mark: add get one, get all, update, delete banks endpoints, validate data in bank dto

--- src/banks/bank.service.ts
+++ src/banks/bank.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BankEntity } from './entity/bank.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { BankDto } from './dto/bank.dto';
-import { from, Observable } from 'rxjs';
+import { from, map, Observable, switchMap } from 'rxjs';
+import { OrderBy } from './enum/order-by.enum';
 
 @Injectable()
 export class BankService {
@@ -14,5 +15,47 @@ export class BankService {
 
   createBank(dto: BankDto): Observable<BankEntity> {
     return from(this.bankRepository.save(dto));
+  }
+
+  findBanks(take: number, skip: number, sortedField: string, orderBy: OrderBy): Observable<BankEntity[]> {
+    return from(
+      this.bankRepository
+        .createQueryBuilder('banks')
+        .orderBy(`banks.${sortedField}`, `${orderBy}`)
+        .take(take)
+        .skip(skip)
+        .getMany(),
+    );
+  }
+
+  findBankById(id: number): Observable<BankEntity> {
+    return from(
+      this.bankRepository.findOne({
+        where: [{ id }],
+      }),
+    ).pipe(
+      map((bank: BankEntity) => {
+        if (!bank) {
+          throw new HttpException('Bank not found', HttpStatus.NOT_FOUND);
+        }
+        return bank;
+      }),
+    );
+  }
+
+  updateBank(id: number, dto: BankDto): Observable<UpdateResult> {
+    return from(this.findBankById(id)).pipe(
+      switchMap((bank: BankEntity) => {
+        return this.bankRepository.update(bank.id, dto);
+      }),
+    );
+  }
+
+  removeBank(id: number): Observable<DeleteResult> {
+    return from(this.findBankById(id)).pipe(
+      switchMap((bank: BankEntity) => {
+        return this.bankRepository.delete(bank.id);
+      }),
+    );
   }
 }
--- src/banks/dto/bank.dto.ts
+++ src/banks/dto/bank.dto.ts
@@ -1,11 +1,14 @@
 import { Currency } from '../enum/currency.enum';
-import { IsEnum, IsNumber, IsString } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class BankDto {
+  @IsOptional()
   @IsString()
   readonly bankName: string;
-  @IsNumber()
+  @IsOptional()
+  @IsNumber({ maxDecimalPlaces: 2 })
   readonly balance: number;
+  @IsOptional()
   @IsEnum(Currency)
   readonly currency: Currency;
 }
